0535a394d86cbeb829c77ddb167e9e219a1b9750
Leptos <leptos.0.null@gmail.com>
2021-05-29 17:24:17 -0700

    [Entries] Replace line breaks with spaces in localized strings

--- Shared/Entries.swift
+++ Shared/Entries.swift
@@ -42,7 +42,9 @@ struct Entries: Codable {
         
         func decodePointsOfInterest(from bundle: Bundle) -> [LocalizedPointOfInterest] {
             pointsOfInterest.map { (timestamp, localizationKey) in
-                let localizedString = bundle.localizedString(forKey: localizationKey, value: nil, table: "Localizable.nocache")
+                let localizedString = bundle
+                    .localizedString(forKey: localizationKey, value: nil, table: "Localizable.nocache")
+                    .replacingOccurrences(of: "\n", with: " ") /* there are some random line breaks that don't seem meaningful */
                 return LocalizedPointOfInterest(id: localizationKey, timestamp: timestamp, value: localizedString)
             }
         }
