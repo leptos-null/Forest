7f3da980b709f82c825d958471e85a69949e0895
Leptos <leptos.0.null@gmail.com>
2021-09-07 15:26:29 -0700

    [PlayerView] Prevent crash if timeStamps isEmpty

--- Shared/UI/PlayerView.swift
+++ Shared/UI/PlayerView.swift
@@ -21,11 +21,13 @@ struct PlayerView {
     var player: AVPlayer {
         let player = AVPlayer(url: url)
         
-        let offsetStamps = timeStamps[1...] + [ .infinity ]
-        let ranges = zip(timeStamps, offsetStamps).map { $0 ..< $1 }
-        player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: nil) { time in
-            let currentSeconds = time.seconds
-            timeStampIndex = ranges.firstIndex { $0.contains(currentSeconds) }
+        if !timeStamps.isEmpty {
+            let offsetStamps = timeStamps[1...] + [ .infinity ]
+            let ranges = zip(timeStamps, offsetStamps).map { $0 ..< $1 }
+            player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: nil) { time in
+                let currentSeconds = time.seconds
+                timeStampIndex = ranges.firstIndex { $0.contains(currentSeconds) }
+            }
         }
         return player
     }
