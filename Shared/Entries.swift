1cea240a9f8b0e8ee75fe69cc35853cbfe9814fc
Leptos <leptos.0.null@gmail.com>
2021-09-09 09:45:14 -0700

    [Entries] Add support for properties added in "15" variant (from "13)

--- Shared/Entries.swift
+++ Shared/Entries.swift
@@ -13,6 +13,8 @@ struct Entries: Codable {
         enum CodingKeys: String, CodingKey {
             case id, accessibilityLabel, pointsOfInterest
             
+            case categories, shotID
+            
             case url_1080_H264 = "url-1080-H264"
             case url_1080_HDR = "url-1080-HDR"
             case url_1080_SDR = "url-1080-SDR"
@@ -23,6 +25,9 @@ struct Entries: Codable {
         let id: UUID
         let accessibilityLabel: String
         let pointsOfInterest: [String: String]
+        
+        let categories: [UUID]? // introduced in tvOS 15
+        let shotID: String?     // introduced in tvOS 15
         
         let url_1080_H264: URL
         let url_1080_HDR: URL
@@ -48,9 +53,33 @@ struct Entries: Codable {
                 return LocalizedPointOfInterest(id: localizationKey, timestamp: timestamp, value: localizedString)
             }
         }
+        
+        func decodeCategories(from entries: Entries) -> [Category]? {
+            guard let entriesCategories = entries.categories,
+                  let assetCategories = categories else { return nil }
+            let categoryLookup = entriesCategories.identifiableLookup
+            return assetCategories.compactMap { categoryLookup[$0] }
+        }
+    }
+    
+    struct Category: Codable, Hashable, Identifiable {
+        let id: UUID
+        let localizedDescriptionKey: String
+        let localizedNameKey: String
+        let preferredOrder: Int
+        let previewImage: URL
+        let representativeAssetID: String
+        
+        func localizedDescription(from bundle: Bundle) -> String {
+            bundle.localizedString(forKey: localizedDescriptionKey, value: nil, table: "Localizable.nocache")
+        }
+        func localizedName(from bundle: Bundle) -> String {
+            bundle.localizedString(forKey: localizedNameKey, value: nil, table: "Localizable.nocache")
+        }
     }
     
     let assets: [Asset]
+    let categories: [Category]? // introduced in tvOS 15
     let initialAssetCount: UInt
     let version: UInt
 }
