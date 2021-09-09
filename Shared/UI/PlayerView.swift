ce80d887721c3b8f45db947e601a94ed9fdc65bb
Leptos <leptos.0.null@gmail.com>
2021-09-08 17:56:12 -0700

    [PlayerView] Improve usage of update in Representable, prevent leak
    of `player.addPeriodicTimeObserver`

--- Shared/UI/PlayerView.swift
+++ Shared/UI/PlayerView.swift
@@ -9,27 +9,46 @@
 import SwiftUI
 import AVKit
 
-struct PlayerView {
+final class PlayerView {
     let url: URL
     let timeStamps: [TimeInterval] // assume sorted
     
     @Binding var timeStampIndex: Int?
     
-    var showsPlaybackControls: Bool = true
-    var videoGravity: AVLayerVideoGravity = .resizeAspect
+    let showsPlaybackControls: Bool
+    let videoGravity: AVLayerVideoGravity
     
-    var player: AVPlayer {
-        let player = AVPlayer(url: url)
-        
-        if !timeStamps.isEmpty {
-            let offsetStamps = timeStamps[1...] + [ .infinity ]
-            let ranges = zip(timeStamps, offsetStamps).map { $0 ..< $1 }
-            player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: nil) { time in
-                let currentSeconds = time.seconds
-                timeStampIndex = ranges.firstIndex { $0.contains(currentSeconds) }
-            }
+    private var playerTimeObserverPair: (player: AVPlayer, timeObserver: Any)? {
+        didSet {
+            guard let previous = oldValue else { return }
+            previous.player.removeTimeObserver(previous.timeObserver)
         }
-        return player
+    }
+    
+    private func addPeriodicTimeObserver(to player: AVPlayer) {
+        guard !timeStamps.isEmpty else { return }
+        
+        let offsetStamps = timeStamps[1...] + [ .infinity ]
+        let ranges = zip(timeStamps, offsetStamps).map { $0 ..< $1 }
+        let observer = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: nil) { [weak self] time in
+            guard let self = self else { return }
+            let currentSeconds = time.seconds
+            self.timeStampIndex = ranges.firstIndex { $0.contains(currentSeconds) }
+        }
+        playerTimeObserverPair = (player, observer)
+    }
+    
+    init(url: URL, timeStamps: [TimeInterval], timeStampIndex: Binding<Int?>,
+         showsPlaybackControls: Bool = true, videoGravity: AVLayerVideoGravity = .resizeAspect) {
+        self.url = url
+        self.timeStamps = timeStamps
+        self._timeStampIndex = timeStampIndex
+        self.showsPlaybackControls = showsPlaybackControls
+        self.videoGravity = videoGravity
+    }
+    
+    deinit {
+        playerTimeObserverPair = nil
     }
 }
 
@@ -38,36 +57,47 @@ struct PlayerView {
 extension PlayerView: NSViewRepresentable {
     func makeNSView(context: Context) -> AVPlayerView {
         let playerView = AVPlayerView()
-        playerView.player = player
-        playerView.controlsStyle = showsPlaybackControls ? .inline : .none
-        playerView.videoGravity = videoGravity
         playerView.updatesNowPlayingInfoCenter = false
-        playerView.showsFullScreenToggleButton = showsPlaybackControls
         playerView.allowsPictureInPicturePlayback = true
         return playerView
     }
     
-    func updateNSView(_ nsView: AVPlayerView, context: Context) {
+    func updateNSView(_ playerView: AVPlayerView, context: Context) {
+        let currentAsset = playerView.player?.currentItem?.asset as? AVURLAsset
+        if currentAsset?.url != url {
+            playerView.player = AVPlayer(url: url)
+        }
+        if let player = playerView.player, playerTimeObserverPair?.player != player {
+            addPeriodicTimeObserver(to: player)
+        }
         
+        playerView.controlsStyle = showsPlaybackControls ? .inline : .none
+        playerView.showsFullScreenToggleButton = showsPlaybackControls
+        playerView.videoGravity = videoGravity
     }
 }
 
 #elseif canImport(UIKit)
 
 extension PlayerView: UIViewControllerRepresentable {
-    
     func makeUIViewController(context: Context) -> AVPlayerViewController {
         let playerController = AVPlayerViewController()
-        playerController.player = player
-        playerController.showsPlaybackControls = showsPlaybackControls
-        playerController.videoGravity = videoGravity
         playerController.updatesNowPlayingInfoCenter = false
         playerController.allowsPictureInPicturePlayback = true
         return playerController
     }
     
-    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+    func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
+        let currentAsset = playerController.player?.currentItem?.asset as? AVURLAsset
+        if currentAsset?.url != url {
+            playerController.player = AVPlayer(url: url)
+        }
+        if let player = playerController.player, playerTimeObserverPair?.player != player {
+            addPeriodicTimeObserver(to: player)
+        }
         
+        playerController.showsPlaybackControls = showsPlaybackControls
+        playerController.videoGravity = videoGravity
     }
 }
 
