11b3e8fee3fd5b5c4c0debe3d6ac7b02654cf722
Leptos <leptos.0.null@gmail.com>
2022-12-08 16:05:34 -0800

    [PlayerView] Convert to be a struct with a Coordinator

--- Shared/UI/PlayerView.swift
+++ Shared/UI/PlayerView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 import AVKit
 
-final class PlayerView {
+struct PlayerView {
     let url: URL
     let timeStamps: [TimeInterval] // assume sorted
     
@@ -21,48 +21,6 @@ final class PlayerView {
     let shouldAutoPlay: Bool
     
     let playerItemEndCallback: (() -> Void)?
-    
-    // MARK: - Periodic Time Observing
-    
-    private var playerTimeObserverPair: (player: AVPlayer, timeObserver: Any)? {
-        didSet {
-            guard let previous = oldValue else { return }
-            previous.player.removeTimeObserver(previous.timeObserver)
-        }
-    }
-    
-    private func addPeriodicTimeObserver(to player: AVPlayer) {
-        guard !timeStamps.isEmpty else { return }
-        
-        let offsetStamps = timeStamps[1...] + [ .infinity ]
-        let ranges = zip(timeStamps, offsetStamps).map { $0 ..< $1 }
-        let observer = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: nil) { [weak self] time in
-            guard let self = self else { return }
-            let currentSeconds = time.seconds
-            self.timeStampIndex = ranges.firstIndex { $0.contains(currentSeconds) }
-        }
-        playerTimeObserverPair = (player, observer)
-    }
-    
-    // MARK: - End Time Observing
-    
-    private let notificationCenter: NotificationCenter = .default
-    private let notificationName: Notification.Name = .AVPlayerItemDidPlayToEndTime
-    
-    private var playerItemObserver: NSObjectProtocol? {
-        didSet {
-            guard let observer = oldValue else { return }
-            notificationCenter.removeObserver(observer, name: notificationName, object: nil)
-        }
-    }
-    
-    private func addEndTimeObserver(to playerItem: AVPlayerItem) {
-        playerItemObserver = notificationCenter.addObserver(forName: notificationName, object: playerItem, queue: .main) { [weak self] note in
-            guard let self = self else { return }
-            self.timeStampIndex = nil
-            self.playerItemEndCallback?()
-        }
-    }
     
     // MARK: -
     
@@ -80,7 +38,7 @@ final class PlayerView {
     
     /// Common routine acting upon player object
     /// - Note: Should only be called from Representable `update` functions
-    private func commonUpdatePlayer(_ player: AVPlayer) {
+    private func commonUpdatePlayer(_ player: AVPlayer, coordinator: Coordinator) {
         let currentAsset = player.currentItem?.asset as? AVURLAsset
         if currentAsset?.url != url {
             player.replaceCurrentItem(with: AVPlayerItem(url: url))
@@ -88,18 +46,78 @@ final class PlayerView {
                 player.play()
             }
         }
-        if playerTimeObserverPair?.player != player {
-            addPeriodicTimeObserver(to: player)
-        }
-        guard let playerItem = player.currentItem else {
-            fatalError("player.currentItem must be valid at this point")
-        }
-        addEndTimeObserver(to: playerItem)
+        coordinator.updatePlayer(player, timeStamps: timeStamps)
     }
     
-    deinit {
-        playerTimeObserverPair = nil
-        playerItemObserver = nil
+    func makeCoordinator() -> Coordinator {
+        Coordinator(timeStampIndex: _timeStampIndex, playerItemEndCallback: playerItemEndCallback)
+    }
+    
+    class Coordinator {
+        @Binding var timeStampIndex: Int?
+        let playerItemEndCallback: (() -> Void)?
+        
+        init(timeStampIndex: Binding<Int?>, playerItemEndCallback: (() -> Void)?) {
+            self._timeStampIndex = timeStampIndex
+            self.playerItemEndCallback = playerItemEndCallback
+        }
+        
+        func updatePlayer(_ player: AVPlayer, timeStamps: [TimeInterval]) {
+            if playerTimeObserver?.player != player || playerTimeObserver?.timeStamps != timeStamps {
+                addPeriodicTimeObserver(for: timeStamps, to: player)
+            }
+            guard let playerItem = player.currentItem else {
+                fatalError("player.currentItem must be valid at this point")
+            }
+            addEndTimeObserver(to: playerItem)
+        }
+        
+        // MARK: - Periodic Time Observing
+        
+        private var playerTimeObserver: (player: AVPlayer, timeObserver: Any, timeStamps: [TimeInterval])? {
+            didSet {
+                guard let previous = oldValue else { return }
+                previous.player.removeTimeObserver(previous.timeObserver)
+            }
+        }
+        
+        private func addPeriodicTimeObserver(for timeStamps: [TimeInterval], to player: AVPlayer) {
+            guard !timeStamps.isEmpty else { return }
+            
+            let offsetStamps = timeStamps[1...] + [ .infinity ]
+            let ranges = zip(timeStamps, offsetStamps).map { ($0)..<($1) }
+            let observer = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: nil) { [weak self] time in
+                guard let self = self else { return }
+                let currentSeconds = time.seconds
+                self.timeStampIndex = ranges.firstIndex { $0.contains(currentSeconds) }
+            }
+            playerTimeObserver = (player, observer, timeStamps)
+        }
+        
+        // MARK: - End Time Observing
+        
+        private let notificationCenter: NotificationCenter = .default
+        private let notificationName: Notification.Name = .AVPlayerItemDidPlayToEndTime
+        
+        private var playerItemObserver: NSObjectProtocol? {
+            didSet {
+                guard let observer = oldValue else { return }
+                notificationCenter.removeObserver(observer, name: notificationName, object: nil)
+            }
+        }
+        
+        private func addEndTimeObserver(to playerItem: AVPlayerItem) {
+            playerItemObserver = notificationCenter.addObserver(forName: notificationName, object: playerItem, queue: .main) { [weak self] note in
+                guard let self = self else { return }
+                self.timeStampIndex = nil
+                self.playerItemEndCallback?()
+            }
+        }
+        // MARK: -
+        deinit {
+            playerTimeObserver = nil
+            playerItemObserver = nil
+        }
     }
 }
 
@@ -120,7 +138,7 @@ extension PlayerView: NSViewRepresentable {
         guard let player = playerView.player else {
             fatalError("playerView.player should always be set in makeNSView")
         }
-        commonUpdatePlayer(player)
+        commonUpdatePlayer(player, coordinator: context.coordinator)
         
         playerView.controlsStyle = showsPlaybackControls ? .inline : .none
         playerView.showsFullScreenToggleButton = showsPlaybackControls
@@ -145,7 +163,7 @@ extension PlayerView: UIViewControllerRepresentable {
         guard let player = playerController.player else {
             fatalError("playerController.player should always be set in makeUIViewController")
         }
-        commonUpdatePlayer(player)
+        commonUpdatePlayer(player, coordinator: context.coordinator)
         
         playerController.showsPlaybackControls = showsPlaybackControls
         playerController.videoGravity = videoGravity
